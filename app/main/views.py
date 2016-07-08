4abdbb57295ac3d6a9305073a8a5c8539f7e601d
ttlttl <wangmingape@gmail.com>
2016-07-08 17:32:52 +0800

    I hate CSS!!!

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, request
 from . import main
+from ..models import House
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    pagination = House.query.order_by(House.timestamp.desc()).paginate(page, per_page=10, error_out=False)
+    posts = pagination.items
+    return render_template('index.html', posts=posts, pagination=pagination, show_used_house=True)
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 # -*- coding=UTF-8 -*-
 import os
 from app import create_app, db
-from app.models import User, District, Area, Community
+from app.models import User, District, Area, Community, House
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -11,7 +11,8 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, District=District, Area=Area, Community=Community)
+    return dict(app=app, db=db, User=User, District=District, Area=Area, Community=Community,\
+                House=House)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
